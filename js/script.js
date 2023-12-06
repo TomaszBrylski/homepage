ba06bd8d92b576a4958679f6e950aa3bafa3ad5c
Tomasz Brylski <brylskitomasz@gmail.com>
2023-12-06 18:31:50 +0100

    initial commit

---
+++ js/script.js
@@ -0,0 +1,13 @@
+console.log("Dzień Dobry Wszystkim");
+
+let changeBackground = document.querySelector(".js-changeBackground");
+let body = document.querySelector(".body");
+let themeName = document.querySelector(".js-themeName");
+
+changeBackground.addEventListener("click", () => {
+  body.classList.toggle("body--dark");
+
+  themeName.innerText = body.classList.contains("body--dark")
+    ? "jasny"
+    : "ciemny";
+});
